602a42f13c81af6e8e6455ca4891927da9c1aebb
Candace <candacelewis@mac.com>
2021-01-07 17:41:23 +0000

    all works, always room for improvement though

--- calcfiles/calcupy.py
+++ calcfiles/calcupy.py
@@ -4,15 +4,18 @@
 message = "\nCalculator in Python, please enter two values (int or decimals okay) and then an operator. \n"
 operators_possible = ["+", "-", "*", "/"]
 
-# function to do the calculation and print result
+# functions to do the calculation and print result
 def calcAdd(val1, val2):
     print("results: ", val1 + val2)
+
 
 def calcMinus(val1, val2):
     print("results: ", val1 - val2)
 
+
 def calcMulti(val1, val2):
     print("results: ", val1 * val2)
+
 
 def calcDiv(val1, val2):
     print("results: ", val1 / val2)
@@ -22,17 +25,22 @@ def userEntry():
     print(message)
     round = 0
     userOps = ""
-    quit = ""
-    while True and quit != "q":
+    while True:
         try:
             val1 = float(input("Please enter the first number: "))
             val2 = float(input("Please enter the second number: "))
             break
         except ValueError:
-            print("Please enter only a decimal or integer value or q to quit")
-    while round < 3 or  userOps != "q":
+            print("Please enter only a decimal or integer value.")
+    while round < 3 and userOps != "q":
         userOps = input("Please enter an operator ( + - * / ) ").strip()
-        if userOps == "+":
+        round += 1
+        if round == 3:
+            print("sorry - you had three tries, comeback later")
+        elif userOps == "q":
+            print("Goodbye.")
+            break
+        elif userOps == "+":
             calcAdd(val1, val2)
             break
         elif userOps == "-":
@@ -44,16 +52,75 @@ def userEntry():
         elif userOps == "/":
             calcDiv(val1, val2)
             break
-        elif userOps == "q":
-            print("Goodbye!")
-            break
         elif userOps not in operators_possible:
             print('oopsy - please enter only + - * /  or q to quit: ')
-            round += 1
-        elif round == 3: 
-            print('sorry - you had three tries, comeback later') 
 
 userEntry()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
